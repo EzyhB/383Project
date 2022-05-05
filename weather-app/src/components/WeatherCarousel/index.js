59176a99c837ae49f98b48cfa8b84f0e78763550
EzyhB <ezyhbmail@gmail.com>
2022-05-05 10:46:17 +0100

    added carousel lib

---
+++ weather-app/src/components/WeatherCarousel/index.js
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+import Carousel from "react-elastic-carousel";
+import React from "react";
+
+export default function WeatherCarousel() {
+  return (
+    <Container>
+      <Carousel></Carousel>
+    </Container>
+  );
+}
