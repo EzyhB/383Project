b4061a674c2b1ef7a3842021f920de6cbd2fa655
EzyhB <ezyhbmail@gmail.com>
2022-05-05 10:41:08 +0100

    added weatherDisplayCard Body

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import WeatherDisplayCard from "./components/WeatherDisplayCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <WeatherDisplayCard day="today" date="idk"></WeatherDisplayCard>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
