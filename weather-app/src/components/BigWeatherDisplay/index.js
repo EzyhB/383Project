90b5f1e11609a74a3042b396601c10d4a9d77c14
EzyhB <ezyhbmail@gmail.com>
2022-05-05 13:43:37 +0100

    big display added and responsive

---
+++ weather-app/src/components/BigWeatherDisplay/index.js
@@ -0,0 +1,17 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+export default function BigWeatherDisplay() {
+  return (
+    <Container
+      sx={{
+        maxWidth: "80%",
+        background: "#fff",
+        borderRadius: "2rem",
+        height: "10rem",
+      }}
+    >
+      <Typography>stuff</Typography>
+    </Container>
+  );
+}
