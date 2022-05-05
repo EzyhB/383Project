5f22dd58355be6b88718bc3d05fa7f1e5678e662
Alex Mulgrew <alexpmulgrew@gmail.com>
2022-05-05 13:04:48 +0100

    search bar added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import WeatherDisplayCard from "./components/WeatherDisplayCard";
 import { Container } from "@mui/material";
 import WeatherCarousel from "./components/WeatherCarousel";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <Container>
+      <SearchBar></SearchBar>
       <WeatherCarousel></WeatherCarousel>
     </Container>
   );
